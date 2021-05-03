983db75dbb9f2ede18246992acc1bc0194a3c335
vitorialira92 <vitorialira92@hotmail.com>
2021-05-02 22:17:17 -0300

    Criação da classe Banco

--- src/Banco.java
+++ src/Banco.java
@@ -1,3 +1,18 @@
 public class Banco {
 
+    private double saldo;
+
+    public Banco(){
+        this.saldo = 0;
+    }
+
+    public void deposito(double valor){
+        saldo += valor;
+    }
+    public void saque(double valor){
+        saldo -= valor;
+    }
+    public double saldo(){
+        return saldo;
+    }
 }
