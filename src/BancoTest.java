ccfcd9d7ec7a000c8d634b9ba4c58dfe2d18b1b5
vitorialira92 <vitorialira92@hotmail.com>
2021-05-02 22:18:45 -0300

    Criação da classe de Test

---
+++ src/BancoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BancoTest {
+
+    @Test
+    void deposito() {
+    }
+
+    @Test
+    void saque() {
+    }
+
+    @Test
+    void saldo() {
+    }
+}
